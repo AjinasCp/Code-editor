0a1ab872c98a8f5c088aeb35852a84ca6c615bcb
AjinasCp <ajinascp111@gmail.com>
2023-10-23 00:57:22 +0530

    new commit

--- script.js
+++ script.js
@@ -1,6 +1,21 @@
-function run(){
-  let htmlCode = document.getElementById("html-code").value;
-  let cssCode = document.getElementById("css-code").value;
-  let jsCode = document.getElementById("js-code").value;
-  let outputCode = document.getElementById("output-code").value;
+
+function run() {
+  var htmlCode = document.getElementById("html-code").value;
+  var cssCode = document.getElementById("css-code").value;
+  var jsCode = document.getElementById("js-code").value;
+  var output = document.getElementById("output");
+
+  var iframeDocument = output.contentDocument;
+  iframeDocument.open();
+  iframeDocument.write(htmlCode);
+
+  var styleTag = document.createElement("style");
+  styleTag.innerHTML = cssCode;
+  iframeDocument.head.appendChild(styleTag);
+
+  var scriptTag = document.createElement("script");
+  scriptTag.innerHTML = jsCode;
+  iframeDocument.body.appendChild(scriptTag);
+
+  iframeDocument.close();
 }
